deda019572861d96b95270552e42a1b5620638dd
yasin Tazeoglu <yasinntazeoglu@gmail.com>
2018-10-18 18:47:31 +0300

    Delete index.js

--- src/components/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Home from './home';
-import Login from './login';
-import Logout from './logout';
-import MemberProfile from './memberProfile';
-
-export {
-  Home, Login, Logout, MemberProfile,
-};
