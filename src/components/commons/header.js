685acfe4683a33763320185b5df2eeec5dce7768
Enes <enes.genc@in2project.com>
2018-10-19 10:44:49 +0300

    fix lint

--- src/components/commons/header.js
+++ src/components/commons/header.js
@@ -1,9 +1,58 @@
-import React, {Component} from 'react';
-import { NavLink} from "react-router-dom";
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+const styles = {
+  navbar: {
+    display: 'flex',
+    alignItems: 'center',
+    minHeight: '4rem',
+    backgroundColor: 'white',
+  },
+  logo: {
+    display: 'inline-block',
+    lineHeight: 'inherit',
+    color: '#637381',
+    textDecoration: 'none',
+    fontWeight: 'bold',
+  },
+  ul: {
+    listStyle: 'none',
+  },
+  li: {
+    display: 'flex',
+    textAlign: '-webkit-match-parent',
+  },
+  link: {
+    color: '#637381',
+    textDecoration: 'none',
+    display: 'block',
+    padding: '.5rem 1rem',
+  },
+  ulLeft: {
+    marginLeft: 'auto !important',
+  },
+  input: {
+    position: 'relative',
+    border: '1px solid #ced4da',
+    padding: '.375rem .75rem',
+    borderRadius: '.25rem',
+    fontSize: '1rem',
+    color: '#637381',
+  },
+  signup: {
+    textDecoration: 'none',
+    fontWeight: 'bold',
+    color: '#637381',
+    padding: '.375rem .75rem',
+    border: '1px solid transparent',
+    borderColor: '#637381',
+    borderRadius: '.25rem',
+  },
+};
 
 class Header extends Component {
-       render(){
-        return (
+  render() {
+    return (
             <header style={styles.navbar}>
                 <NavLink to="/" style={styles.logo}>Kommunity.App</NavLink>
                 <ul style={styles.ul}>
@@ -21,57 +70,8 @@ class Header extends Component {
                     </li>
                 </ul>
             </header>
-        );
-    }
+    );
+  }
 }
-
-const styles = {
-    navbar: {
-       display: 'flex',
-       alignItems: 'center',
-       minHeight: '4rem',
-       backgroundColor: 'white',
-    },    
-    logo: {
-        display: 'inline-block',
-        lineHeight: 'inherit',
-        color: '#637381',
-        textDecoration: 'none',
-        fontWeight: 'bold'
-    },
-    ul: {
-        listStyle: 'none',
-    },
-    li: {
-        display: 'flex',
-        textAlign: '-webkit-match-parent'
-    },
-    link: {
-        color: '#637381',
-        textDecoration: 'none',
-        display: 'block',
-        padding: '.5rem 1rem'
-    },
-    ulLeft: {
-        marginLeft: 'auto !important'
-    },
-    input : {
-        position: 'relative',
-        border: '1px solid #ced4da',
-        padding: '.375rem .75rem',
-        borderRadius: '.25rem',
-        fontSize: '1rem',
-        color: '#637381',
-    },
-    signup: {
-        textDecoration: 'none',
-        fontWeight: 'bold',
-        color: '#637381',
-        padding: '.375rem .75rem',
-        border: '1px solid transparent',
-        borderColor: '#637381',
-        borderRadius: '.25rem',
-    },
-   }
 
 export default Header;
