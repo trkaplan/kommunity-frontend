d1a42a9b9d69c18d01fdec3edfddf2fe48b4bd2a
Enes <enes.genc@in2project.com>
2018-10-19 10:39:50 +0300

    add header

---
+++ src/components/commons/header.js
@@ -0,0 +1,77 @@
+import React, {Component} from 'react';
+import { NavLink} from "react-router-dom";
+
+class Header extends Component {
+       render(){
+        return (
+            <header style={styles.navbar}>
+                <NavLink to="/" style={styles.logo}>Kommunity.App</NavLink>
+                <ul style={styles.ul}>
+                    <li style={styles.li}>
+                        <NavLink to="/" style={styles.link}>Features</NavLink>
+                        <NavLink to="/" style={styles.link}>Solutions</NavLink>
+                        <NavLink to="/" style={styles.link}>Communities</NavLink>
+                    </li>
+                </ul>
+             <ul style={styles.ulLeft}>
+                    <li style={styles.li}>
+                        <input style={styles.input} placeholder="Search Communities"/>
+                        <NavLink to="/login" style={styles.link}>Login</NavLink>
+                        <NavLink to="/" style={styles.signup}>Sign Up</NavLink>
+                    </li>
+                </ul>
+            </header>
+        );
+    }
+}
+
+const styles = {
+    navbar: {
+       display: 'flex',
+       alignItems: 'center',
+       minHeight: '4rem',
+       backgroundColor: 'white',
+    },    
+    logo: {
+        display: 'inline-block',
+        lineHeight: 'inherit',
+        color: '#637381',
+        textDecoration: 'none',
+        fontWeight: 'bold'
+    },
+    ul: {
+        listStyle: 'none',
+    },
+    li: {
+        display: 'flex',
+        textAlign: '-webkit-match-parent'
+    },
+    link: {
+        color: '#637381',
+        textDecoration: 'none',
+        display: 'block',
+        padding: '.5rem 1rem'
+    },
+    ulLeft: {
+        marginLeft: 'auto !important'
+    },
+    input : {
+        position: 'relative',
+        border: '1px solid #ced4da',
+        padding: '.375rem .75rem',
+        borderRadius: '.25rem',
+        fontSize: '1rem',
+        color: '#637381',
+    },
+    signup: {
+        textDecoration: 'none',
+        fontWeight: 'bold',
+        color: '#637381',
+        padding: '.375rem .75rem',
+        border: '1px solid transparent',
+        borderColor: '#637381',
+        borderRadius: '.25rem',
+    },
+   }
+
+export default Header;
