3cfb8db92c4f2e6ac053c4c9326ab4eb98eb65ae
Enes <enes.genc@in2project.com>
2018-10-23 21:15:58 +0300

    comminities page test added.

--- test/components/app.test..js
+++ test/components/app.test..js
@@ -3,6 +3,7 @@ import MemoryRouter from 'react-router-dom/MemoryRouter';
 
 import { shallow } from 'enzyme';
 import App from '@/components/app';
+import Communities from '@/components/pages/communities';
 
 describe('Components: <App />', () => {
   test('renders without exploding', () => {
@@ -12,5 +13,10 @@ describe('Components: <App />', () => {
       </MemoryRouter>,
     );
     expect(wrapper.html()).toContain('header');
+  });
+  // Communities test
+  test('communities render', () => {
+    const wrapper = shallow(<Communities />);
+    expect(wrapper.html().toContain('div'));
   });
 });
