19911e040da725dbc022f03f538bc8f338ba6974
Selman Kahya <selmankahya@hotmail.com>
2018-10-24 21:14:18 -0700

    Update header.test.js

--- test/components/ui/header.test.js
+++ test/components/ui/header.test.js
@@ -3,17 +3,19 @@ import { shallow } from 'enzyme';
 import UIHeader from '@/components/ui/header';
 
 describe('Component: <UIHeader />', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(
-      <UIHeader type='h1' className='testClass' text='This is h1 type header'/>,
-    );
-  });
+  const wrapper = shallow(
+    <UIHeader type='h1' className='testClass' text='TITLE'/>,
+  );
+  
   test('has classname', () => {
     expect(wrapper.html()).toContain('class="testClass"');
   });
+  
   test('has correct tag', () => {
     expect(wrapper.html()).toContain('h1');
+  });
+  
+  test('has text', () => {
+    expect(wrapper.text()).toBe('TITLE');
   });
 });
